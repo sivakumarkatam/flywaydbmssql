e98a6556a81829ed982d276fa4847df7c1ccefcd
sivakumarkatam <siva.katam@blazeclan.com>
2017-10-05 10:31:00 +0530

    Update startapp.bash

--- deploymentscripts/startapp.bash
+++ deploymentscripts/startapp.bash
@@ -5,5 +5,5 @@
 #aws --region ap-southeast-1  ssm get-parameters --names "Dbpassword" | awk 'match($0, /"Value":.*/) { print substr($0, RSTART, RLENGTH) }'| cut -d '"' -f4 | sudo tee pwd
 #password=$(cat pwd)
 cd /tmp/
-mvn install flyway:migrate
+mvn compile flyway:migrate
 # -Ddatabaseuser=$username -Ddatabasepassword=$password
