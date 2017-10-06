1a36e347b204811e9fc414efb9c878f3820ea2ce
sivakumarkatam <siva.katam@blazeclan.com>
2017-10-06 14:17:00 +0530

    Update V3__New_table.sql

--- src/main/resources/db/migration/V3__New_table.sql
+++ src/main/resources/db/migration/V3__New_table.sql
@@ -8,8 +8,8 @@ CREATE TABLE vegetables (
 INSERT INTO vegetables
     (vegtables_id, vegtables_name, vegtables_price)
 VALUES 
-    (1, "potato", 50),
-    (2, "Bringel", 10),
-    (3, "Tomatoes", 20),
-    (4, "carrot", 20)
+    (1, 'potato', 50),
+    (2, 'Bringel', 10),
+    (3, 'Tomatoes', 20),
+    (4, 'carrot', 20)
 GO
