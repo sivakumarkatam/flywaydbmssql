32817da95b1d8da69637ec0777568deb160df1c6
sivakumarkatam <siva.katam@blazeclan.com>
2017-10-25 16:17:18 +0530

    Update startapp.bash

--- deploymentscripts/startapp.bash
+++ deploymentscripts/startapp.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-aws --region ap-southeast-1  ssm get-parameters --names "flyuser" | awk 'match($0, /"Value":.*/) { print substr($0, RSTART, RLENGTH) }'| cut -d '"' -f4 | sudo tee user
+aws --region ap-southeast-1 ssm get-parameters --names "flyuser" --with-decryption | awk 'match($0, /"Value":.*/) { print substr($0, RSTART, RLENGTH) }'| cut -d '"' -f4 | sudo tee user
 username=$(cat user)
 
-aws --region ap-southeast-1  ssm get-parameters --names "flypassword" | awk 'match($0, /"Value":.*/) { print substr($0, RSTART, RLENGTH) }'| cut -d '"' -f4 | sudo tee pwd
+aws --region ap-southeast-1 ssm get-parameters --names "flypassword" --with-decryption | awk 'match($0, /"Value":.*/) { print substr($0, RSTART, RLENGTH) }'| cut -d '"' -f4 | sudo tee pwd
 password=$(cat pwd)
 cd /tmp/
 mvn compile flyway:migrate -Ddatabaseuser=$username -Ddatabasepassword=$password
